40c9459a5ccf214f52abccd7953908dcb0ce6e03
Rmehta26 <rashmi.mehta.23cse@bmu.edu.in>
2024-12-01 23:55:27 +0530

    Add files via upload

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+ocument.addEventListener("DOMContentLoaded", () => {
     
     const style = document.createElement("style");
     style.textContent = `
